72d31e39bb969af6aa1aff9dc35c2d083047279c
Vikas Prajapati <vp670488@gmail.com>
2024-10-07 13:30:39 +0530

    Added Prime Functions

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let arr = [19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30];
 let n = arr.length;
-let count = 0;
-for (let i=1; i<n; i++)
+let ansArr = [];
+
+for (let i=0;i<n;i++)
 {
-    for (let j = 1;j<=arr[i];j++)
+    let count = 0;
+    for (let j=1;j<=arr[i];j++)
     {
         if (arr[i] % j === 0)
         {
@@ -12,6 +14,7 @@ for (let i=1; i<n; i++)
     }
     if (count == 2)
     {
-        console.log(arr[i], "is a prime number");
+        ansArr.push(arr[i]);
     }
 }
+console.log(ansArr);
--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-// let arr = [1,2,3,4,5];
-// let numbers = [3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18];
-// let n = numbers.length;
-// for (let i=0;i<n;i++)
-// {
-//     if (numbers[i] % 2 === 0)
-//     {
-//         console.log(numbers[i],"Is an Even Number");
-//     }
-//     else
-//     {
-//         console.log(numbers[i], "Is an Odd Number");
-//     }
-// }
-
-let arr = [19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30];
-let n = arr.length;
-let count = 0;
-for (let i=1; i<n; i++)
+let arr = [1,2,3,4,5];
+let numbers = [3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18];
+let n = numbers.length;
+for (let i=0;i<n;i++)
 {
-    for (let j = 1;j<=arr[i];j++)
+    if (numbers[i] % 2 === 0)
     {
-        if (arr[i] % j === 0)
-        {
-            count++;
-        }
+        console.log(numbers[i],"Is an Even Number");
     }
-    if (count == 2)
+    else
     {
-        console.log(arr[i], "is a prime number");
+        console.log(numbers[i], "Is an Odd Number");
     }
 }
+
